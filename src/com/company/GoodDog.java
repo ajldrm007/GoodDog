6a201840f7931fdc8ab0c404f016bdb19a6477c4
andy <ajldrm007@gmail.com>
2019-04-16 23:46:43 -0400

    finishing up GoodDog and Main

--- src/com/company/GoodDog.java
+++ src/com/company/GoodDog.java
@@ -3,4 +3,22 @@ package com.company;
 public class GoodDog {
     private int size;
 
+    public int getSize(){
+        return size;
+    }
+
+    public void setSize(int s){
+        size = s;
+    }
+
+    void bark(){
+        if(size>60){
+            System.out.println("Woof! Woof!");
+        }else if (size>14){
+            System.out.println("Ruff! Ruf!");
+        }else{
+            System.out.println("Yip! Yip!");
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         GoodDog one = new GoodDog();
+        one.setSize(70);
+        GoodDog two = new GoodDog();
+        two.setSize(8);
+        System.out.println("Dog one: " + one.getSize());
+        System.out.println("Dog two: " + two.getSize());
+        one.bark();
+        two.bark();
     }
 }
